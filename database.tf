690b2a1c24401c1637bfebb5a0cdfda376f190ab
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-01-26 21:02:15 -0500

    Remove conditional count for database user creation

--- database.tf
+++ database.tf
@@ -1,5 +1,4 @@
 resource "digitalocean_database_user" "dbuser" {
-  count      = var.database_user == "doadmin" ? 0 : 1
   cluster_id = digitalocean_database_cluster.k3s.id
   name       = var.database_user
 }
