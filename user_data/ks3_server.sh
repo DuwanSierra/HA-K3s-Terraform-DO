dbd8346547b4524d29e69612b88bbd75e523d5ce
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-02-03 19:54:46 -0500

    Refactor k3s configuration setup by consolidating flannel arguments and improving readability

--- user_data/ks3_server.sh
+++ user_data/ks3_server.sh
@@ -7,25 +7,39 @@ apt-get install -yq \
     ntp \
     wireguard
 
-# Store Droplet ID in variable (utilises DO's Metadata Service - https://developers.digitalocean.com/documentation/metadata/)
 DROPLET_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
 
-# Configurar flags de k3s basado en flannel_backend
-if [ "${flannel_backend}" = "none" ]; then
-    K3S_FLANNEL_ARGS="--flannel-backend=none --disable-network-policy"
-else
-    K3S_FLANNEL_ARGS="--flannel-backend=${flannel_backend} --flannel-iface=eth1"
+# Crear directorio de configuración
+mkdir -p /etc/rancher/k3s
+
+# Crear archivo de configuración
+cat > /etc/rancher/k3s/config.yaml <<EOF
+datastore-endpoint: "${db_cluster_uri}"
+tls-san:
+  - "${k3s_lb_ip}"
+disable:
+  - local-storage
+  - servicelb
+  - traefik
+  - cloud-controller
+disable-cloud-controller: true
+flannel-backend: "${flannel_backend}"
+disable-network-policy: $([ "${flannel_backend}" = "none" ] && echo "true" || echo "false")
+kubelet-arg:
+  - "provider-id=digitalocean://$DROPLET_ID"
+  - "cloud-provider=external"
+EOF
+
+# Solo agregar flannel-iface si no es "none"
+if [ "${flannel_backend}" != "none" ]; then
+    echo "flannel-iface: eth1" >> /etc/rancher/k3s/config.yaml
 fi
 
-# k3s
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - server \
-    --datastore-endpoint="${db_cluster_uri}" \
-    ${critical_taint} \
-    --kubelet-arg "provider-id=digitalocean://$DROPLET_ID" \
-    --tls-san ${k3s_lb_ip} \
-    $K3S_FLANNEL_ARGS \
-    --disable local-storage \
-    --disable-cloud-controller \
-    ${enable_traefik} \
-    --disable servicelb \
-    --kubelet-arg 'cloud-provider=external'
+# Solo agregar node-taint si está definido
+if [ -n "${critical_taint}" ] && [ "${critical_taint}" != "" ]; then
+    echo "node-taint:" >> /etc/rancher/k3s/config.yaml
+    echo "  - CriticalAddonsOnly=true:NoExecute" >> /etc/rancher/k3s/config.yaml
+fi
+
+# Instalar k3s
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - server
