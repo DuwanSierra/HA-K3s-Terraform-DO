dec7b878c4fd0caa4e3b1daea2515d1f232fb87b
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-02-03 21:36:21 -0500

    Add tls-san configuration to k3s installation scripts

--- user_data/ks3_server.sh
+++ user_data/ks3_server.sh
@@ -17,11 +17,17 @@ else
     K3S_FLANNEL_ARGS="--flannel-backend=${flannel_backend} --flannel-iface=eth1"
 fi
 
+# Escribir configuración de k3s
+install -d /etc/rancher/k3s
+cat > /etc/rancher/k3s/config.yaml <<EOF
+tls-san:
+  - "${k3s_lb_ip}"
+EOF
+
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - server \
     --datastore-endpoint="${db_cluster_uri}" \
     ${critical_taint} \
-    --tls-san ${k3s_lb_ip} \
     --kubelet-arg "provider-id=digitalocean://$DROPLET_ID" \
     $K3S_FLANNEL_ARGS \
     --disable local-storage \
