aabe2b081f26ca9ec443021814e70417ecc6bc91
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-02-03 21:12:25 -0500

    Fix quoting for --tls-san argument in k3s installation script

--- user_data/ks3_server.sh
+++ user_data/ks3_server.sh
@@ -21,7 +21,7 @@ fi
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - server \
     --datastore-endpoint="${db_cluster_uri}" \
     ${critical_taint} \
-    --tls-san "${k3s_lb_ip}" \
+    --tls-san ${k3s_lb_ip} \
     --kubelet-arg "provider-id=digitalocean://$DROPLET_ID" \
     $K3S_FLANNEL_ARGS \
     --disable local-storage \
