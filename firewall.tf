99b55d1105d4921e991a1d92df50293ee1a36d74
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-03-04 18:09:01 -0500

    fix: add missing tags to ccm_firewall resource in firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,6 +1,7 @@
 resource "digitalocean_firewall" "ccm_firewall" {
   name = "ccm-firewall"
 
+  tags = [digitalocean_tag.server.name, digitalocean_tag.agent.name]
   outbound_rule {
     protocol              = "icmp"
     destination_addresses = ["0.0.0.0/0", "::/0"]
