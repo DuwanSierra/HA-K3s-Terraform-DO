6e6d1ec33ae77e010f34cadf9abb000f0a7b2389
desierrag@gmail.com <Duwan Estiven Sierra Guerrero>
2026-03-04 19:56:26 -0500

    feat: add PRIVATE_IP and PUBLIC_IP variables to k3s scripts for improved network configuration

--- user_data/ks3_server_init.sh
+++ user_data/ks3_server_init.sh
@@ -8,6 +8,8 @@ apt-get install -yq \
 
 # Store Droplet ID in variable (utilises DO's Metadata Service - https://developers.digitalocean.com/documentation/metadata/)
 DROPLET_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
+PRIVATE_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 
 # Escribir configuración de k3s
 install -d /etc/rancher/k3s
@@ -22,6 +24,9 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3
     ${critical_taint} \
     --kubelet-arg="provider-id=digitalocean://$DROPLET_ID" \
     --flannel-iface=eth1 \
+    --node-ip=$PRIVATE_IP \
+    --advertise-address=$PRIVATE_IP \
+    --node-external-ip=$PUBLIC_IP \
     --disable local-storage \
     --disable servicelb \
     --disable-cloud-controller \
